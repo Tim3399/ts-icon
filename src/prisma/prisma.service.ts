f154a7fe3c342ba71ea3db5fe56f7b5a3c7a6e83
Tim <timratermann@web.de>
2025-06-08 22:40:08 +0200

    logging db

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,6 +4,15 @@ import { PrismaClient } from '@prisma/client'
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+
+    this.$use(async (params, next) => {
+
+      if (['create', 'update', 'delete', 'upsert'].includes(params.action)) {
+        console.log(`[Prisma] ${params.model}.${params.action}`, params.args)
+      }
+      return next(params)
+    })
+
     await this.$connect()
   }
 }
