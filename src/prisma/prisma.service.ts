2ed52833009c0a0c5ed82ca1d9bcc3f353c82c4c
Tim <timratermann@web.de>
2025-06-08 22:42:30 +0200

    prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,26 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 
+function sanitizeArgs(args: any): any {
+  if (!args || typeof args !== 'object') return args
+  const copy = Array.isArray(args) ? [...args] : { ...args }
+  for (const key in copy) {
+    if (Buffer.isBuffer(copy[key])) {
+      copy[key] = `<Buffer (${copy[key].length} bytes)>`
+    } else if (typeof copy[key] === 'object') {
+      copy[key] = sanitizeArgs(copy[key])
+    }
+  }
+  return copy
+}
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-
     this.$use(async (params, next) => {
-
       if (['create', 'update', 'delete', 'upsert'].includes(params.action)) {
-        console.log(`[Prisma] ${params.model}.${params.action}`, params.args)
+        const sanitizedArgs = sanitizeArgs(params.args)
+        console.log(`[Prisma] ${params.model}.${params.action}`, sanitizedArgs)
       }
       return next(params)
     })
