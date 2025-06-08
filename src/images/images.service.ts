09a6d1349539c73ada71f54bd661cc4e00dbd2b3
Tim <timratermann@web.de>
2025-06-08 22:52:16 +0200

    getimg change

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -5,10 +5,21 @@ import { PrismaService } from '../prisma/prisma.service'
 export class ImagesService {
   constructor(private prisma: PrismaService) {}
 
-  getImage(channelName: string) {
-    return this.prisma.channelImage.findUnique({
+  async getImage(channelName: string): Promise<{ image: Buffer; mimeType: string } | null> {
+    const result = await this.prisma.channelImage.findUnique({
       where: { channelName },
-    })
+      select: {
+        image: true,
+        mimeType: true,
+      },
+    });
+
+    if (!result) return null;
+
+    return {
+      image: Buffer.from(result.image),
+      mimeType: result.mimeType,
+    };
   }
 
   async saveImage(channelName: string, buffer: Buffer, mimeType: string) {
