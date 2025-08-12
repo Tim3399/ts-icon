44f8951825456ee4495e553cfa1c703c3ad72c68
Tim <timratermann@web.de>
2025-08-12 19:26:20 +0200

    minor fixes for imports

--- src/app.module.local.ts
+++ src/app.module.local.ts
@@ -1,8 +1,13 @@
+
 import { Module } from '@nestjs/common'
 import { PrismaModule } from './prisma/prisma.module'
 import { ImagesModuleLocal } from './images/images.module.local'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
   imports: [PrismaModule, ImagesModuleLocal],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
