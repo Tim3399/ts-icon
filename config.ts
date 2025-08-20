22e5539bde0bd64c8b27c6a7ee4f112933f230af
Tim <timratermann@web.de>
2025-08-20 11:06:16 +0200

    format

--- config.ts
+++ config.ts
@@ -8,8 +8,8 @@ export const CORS_ORIGINS = (process.env.CORS_ORIGINS || '')
   .map(origin => origin.trim())
   .filter(Boolean);
 
-  export const DATABASE_URL = process.env.DATABASE_URL || 'file:./dev.db';
-  console.log("Database URL is set to:", DATABASE_URL);
+export const DATABASE_URL = process.env.DATABASE_URL || 'file:./dev.db';
+console.log("Database URL is set to:", DATABASE_URL);
 
 export const TS_HOST = process.env.TS_HOST || 'localhost';
 export const TS_QUERY_PORT = Number(process.env.TS_QUERY_PORT) || 10011;
